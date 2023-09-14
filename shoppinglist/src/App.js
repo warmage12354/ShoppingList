adcaa26eed545c05e100539a1ab0778c178256df
Briam <boomyoman@gmail.com>
2023-09-13 19:09:36 -0500

    Made simple Grocery List

---
+++ shoppinglist/src/App.js
@@ -0,0 +1,82 @@
+import React from 'react'; 
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+    const [list,setList] = useState([])
+    const [inputData,setInputData] = useState('');
+
+
+    const handleAddItem = () => { 
+
+        const newList = [...list,{title:inputData}]
+        setList(newList) 
+        setInputData('') 
+        
+
+    }
+
+    const handleRemoveItem = (index) => { 
+
+        const newList =  []; 
+
+        for (let i = 0; i < list.length; i++) { 
+
+            if(index != i) { 
+
+                newList.push(list[i])
+
+
+            }            
+
+
+
+        }
+
+        setList(newList)
+
+    }
+
+
+
+  return (
+  
+   <div className='App'>
+       
+       <h1>Grocery List</h1>
+
+       <div className = 'input'> 
+        <input type='text' value={inputData} onChange= {(event) => setInputData(event.target.value)}></input>
+        <input type = 'button' value="ADD" onClick = {() => handleAddItem()}></input>
+
+       </div>
+
+        <div className='list'> 
+        
+        {list.map((item,index) => { 
+
+            return ( 
+
+                <div> 
+                    <p onClick = { () => handleRemoveItem(index) } >{item.title}</p>
+
+                    </div>
+
+            )
+
+
+        })}
+
+
+        
+        
+        </div>
+    </div>
+
+  )
+
+
+}
+
+export default App;
