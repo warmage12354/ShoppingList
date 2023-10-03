f7b0851a052f50e197d6ee9c61cb7bfc067d5f63
Briam <boomyoman@gmail.com>
2023-10-02 19:17:13 -0500

    Minor edits

--- server/db.js
+++ server/db.js
@@ -3,8 +3,10 @@ const app = express();
 const mysql = require('mysql2'); 
 const cors =  require('cors');
 
+
 app.use(cors());
 app.use(express.json());
+
 
 const db = mysql.createConnection({
 host: "localhost",
